0aa4ae4c4fd53c49268c1dc7a3f594385798eb8f
petr <petrsupreme@gmail.com>
2020-01-29 21:25:08 +0200

    market edited

--- src/ua/com/alevel/Notebook.java
+++ src/ua/com/alevel/Notebook.java
@@ -1,7 +1,47 @@
 package ua.com.alevel;
 
 public class Notebook {//верисия маркетинг
-    public static void main(String[] args) {
+   private String name;
+   private double priceUSD;
+   private double weightKG;
+   private double displaySizeInch;
 
+    public Notebook(String name, double priceUSD, double weightKG, double displaySizeInch) {
+        this.name = name;
+        this.priceUSD = priceUSD;
+        this.weightKG = weightKG;
+        this.displaySizeInch = displaySizeInch;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPriceUSD() {
+        return priceUSD;
+    }
+
+    public void setPriceUSD(double priceUSD) {
+        this.priceUSD = priceUSD;
+    }
+
+    public double getWeightKG() {
+        return weightKG;
+    }
+
+    public void setWeightKG(double weightKG) {
+        this.weightKG = weightKG;
+    }
+
+    public double getDisplaySizeInch() {
+        return displaySizeInch;
+    }
+
+    public void setDisplaySizeInch(double displaySizeInch) {
+        this.displaySizeInch = displaySizeInch;
     }
 }
